ab65c8e2fa039f855da1761779cf738da02fc379
Pasindu Weerasinghe <pasinduweerasinghe69@gmail.com>
2025-08-27 17:03:11 +0530

    Commit_02

--- Frount-end/src/App.js
+++ Frount-end/src/App.js
@@ -1,16 +1,21 @@
-
+import Login from './pages/Login';
+import Register from './pages/Register';
 import './App.css';
+import {BrowserRouter , Route , Routes} from "react-router-dom";
+
+//let value = {user , signin , signout};
+
 
 function App() {
   return (
-    <AuthContext.Provider value={value}>
+   // <AuthContext.Provider value={value}>
       <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes> 
       </BrowserRouter>
-    </AuthContext.Provider>
+   // </AuthContext.Provider>
   );
 }
 
